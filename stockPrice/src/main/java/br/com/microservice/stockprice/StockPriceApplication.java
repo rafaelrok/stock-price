0285053a0374aea8118a348edaae8feff6fcdf54
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-09-19 01:50:26 -0300

    Created project rabbitmq-microservice

---
+++ stockPrice/src/main/java/br/com/microservice/stockprice/StockPriceApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.stockprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockPriceApplication.class, args);
+    }
+
+}
