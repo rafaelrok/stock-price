94f97afaeab895da74b5229980289a6925f99e55
Rafael Vieira dos Santos <rafaelvieirasantos2006@hotmail.com>
2021-09-19 11:20:45 -0300

    Connections and Constantes

---
+++ stockPrice/src/main/java/br/com/microservice/stockprice/constantes/RabbitMQConstantes.java
@@ -0,0 +1,6 @@
+package br.com.microservice.stockprice.constantes;
+
+public class RabbitMQConstantes {
+    public static final String FILA_ESTOPQUE =  "ESTOQUE";
+    public static final String FILA_PRECO =  "PRECO";
+}
